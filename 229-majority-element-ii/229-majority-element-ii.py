789fa1839eb4f50bcefe957628f3b81e78ad10b8
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-06-04 02:32:02 +0530

    Time: 124 ms (84.27%), Space: 15.4 MB (23.65%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.py
+++ 229-majority-element-ii/229-majority-element-ii.py
@@ -1,22 +1,24 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         
-        elem1 = elem2 =None 
-        freq1 = freq2 = 0 
+        elem1 = elem2 = 0
+        c1 = c2 = 0 
+        
         for val in nums:
+        
             if val == elem1:
-                freq1 += 1
+                c1 += 1
             elif val == elem2:
-                freq2 += 1
-            elif freq1 == 0:
+                c2 += 1
+            elif c1 == 0:
                 elem1 = val
-                freq1 = 1
-            elif freq2 == 0:
+                c1=1
+            elif c2 == 0:
                 elem2 = val 
-                freq2 = 1
+                c2 = 1
             else:
-                freq1 -= 1
-                freq2 -= 1
+                c1 -= 1
+                c2 -= 1
                 
         count1 = count2 = 0
         for val in nums:
