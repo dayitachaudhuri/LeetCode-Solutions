10cc4e067801f79d324a9fc73d8ecd9ca6156d57
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-06-04 03:16:53 +0530

    Time: 27 ms (97.13%), Space: 13.9 MB (73.71%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,9 +1,6 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        
-        matrix=[[1]*n]*m
+        res=1
         for i in range(1,m):
-            for j in range(1,n):
-                matrix[i][j]=matrix[i-1][j]+matrix[i][j-1]
-                
-        return matrix[m-1][n-1]
+            res=res*(n-1+i)//i;
+        return res
