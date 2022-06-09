838d5d09a9701193481a5673d86af33028a6dd58
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-06-10 01:33:14 +0530

    Time: 89 ms (23.11%), Space: 20.3 MB (5.26%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        arr=[x.lower() for x in list(s) if x.isalnum()]
+        for i in range(len(arr)//2):
+            if arr[i]!=arr[len(arr)-i-1]:
+                return False
+        return True
