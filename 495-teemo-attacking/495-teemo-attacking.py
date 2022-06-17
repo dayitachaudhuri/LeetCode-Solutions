478103be32ed5bcfe9b4a422514b8d151951594c
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-06-18 02:10:00 +0530

    Time: 260 ms (95.75%), Space: 15.5 MB (47.63%) - LeetHub

--- 495-teemo-attacking/495-teemo-attacking.py
+++ 495-teemo-attacking/495-teemo-attacking.py
@@ -1,9 +1,10 @@
 class Solution:
     def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
-        tot=duration
-        for i in range(0,len(timeSeries)-1):
-            if timeSeries[i+1]-timeSeries[i]<duration:
-                tot+=timeSeries[i+1]-timeSeries[i]
-            else:
-                tot+=duration
-        return tot
+        last = -duration
+        res = 0
+        for time in timeSeries:
+            if time - last < duration:
+                res -= last + duration - time
+            res += duration
+            last = time
+        return res
