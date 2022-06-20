cf303dcbf7dcd6f0e3b7ee7d9007b33735e6ba90
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-06-21 03:22:38 +0530

    Time: 80 ms (67.15%), Space: 14.3 MB (82.22%) - LeetHub

--- 51-n-queens/51-n-queens.py
+++ 51-n-queens/51-n-queens.py
@@ -1,18 +1,30 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         res=[]
+        col=[]
+        posDiagonal=[]
+        negDiagonal=[]
+        board=[["."]*n for i in range(n)]
         
-        def helper(board,r,col,posDiagonal,negDiagonal):
+        def helper(r):
             if r==n:
                 res.append(["".join(row) for row in board])
                 return
             for c in range(n):
                 if c in col or (r+c) in posDiagonal or (r-c) in negDiagonal:
                     continue
+                    
+                col.append(c)
+                posDiagonal.append(r+c)
+                negDiagonal.append(r-c)
                 board[r][c]="Q"
-                helper(board,r+1,col+[c],posDiagonal+[r+c],negDiagonal+[r-c])
+                
+                helper(r+1)
+                
+                col.pop()
+                posDiagonal.pop()
+                negDiagonal.pop()
                 board[r][c]="."
                 
-        board=[["."]*n for i in range(n)]
-        helper(board,0,[],[],[])
+        helper(0)
         return res
