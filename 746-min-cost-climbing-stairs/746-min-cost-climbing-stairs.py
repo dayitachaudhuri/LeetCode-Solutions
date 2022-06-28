8585f40ab2b64bdb93bd0d6c09c9c221fc128c07
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-06-29 02:27:35 +0530

    Time: 67 ms (83.26%), Space: 13.9 MB (97.14%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,8 +1,10 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        cache={0:cost[0],1:cost[1]}
         nx=len(cost)
+        prev1=cost[1]
+        prev2=cost[0]
         for i in range(2,nx):
-            cache[i]=cost[i]+min(cache[i-1],cache[i-2])
-        
-        return min(cache[nx-1],cache[nx-2])
+            curr=cost[i]+min(prev1,prev2)
+            prev2=prev1
+            prev1=curr
+        return min(prev1,prev2)
