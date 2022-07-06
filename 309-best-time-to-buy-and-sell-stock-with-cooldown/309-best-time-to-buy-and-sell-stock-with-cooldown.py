1ff05fc3f9d13c1454e0ce2b0bd4bb2068446fdc
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-07-06 23:40:49 +0530

    Time: 94 ms (17.94%), Space: 17.9 MB (30.77%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -5,15 +5,25 @@ class Solution:
         dp = {}
         
         def dfs(i,hold):
+            
+            # Base Cases
             if i>=n:
                 return 0
             if (i,hold) in dp:
                 return dp[(i,hold)]
+            
+            # If holding stock, we can sell it and wait the coldown.
             if hold:
                 action = dfs(i+2,not hold) + prices[i]
+                
+            # If not holding stock, we can buy it.
             else:
                 action = dfs(i+1,not hold) - prices[i]
+                
+            # We can wait.
             wait = dfs(i+1, hold)
+            
+            # Update the hashmap with maximum of selling/buying and waiting.
             dp[(i,hold)] = max(action,wait)
             return dp[(i,hold)]
     
