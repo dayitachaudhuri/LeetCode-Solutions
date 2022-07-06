a3d228693cbe9b92dd8665d1cba28568718fdbd5
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-07-07 01:55:51 +0530

    Time: 93 ms (18.62%), Space: 14.5 MB (49.31%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -4,7 +4,11 @@ class Solution:
         dp=[[0,0] for i in range(0,len(prices)+2)]
         
         for i in range(len(prices)-1,-1,-1):
+            
+            # For Hold=False, max(buy,wait)
             dp[i][0]=max(dp[i+1][1]-prices[i],dp[i+1][0])
+            
+            # For Hold=True, max(sell,wait)
             dp[i][1]=max(dp[i+2][0]+prices[i],dp[i+1][1])
         
         return dp[0][0]
