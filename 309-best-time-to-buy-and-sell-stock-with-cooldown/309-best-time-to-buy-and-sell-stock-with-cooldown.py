87590c77980da0d55a6613d8187233a00970278d
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-07-07 01:54:45 +0530

    Time: 56 ms (72.89%), Space: 14.5 MB (52.20%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,30 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
-        n = len(prices)
-        dp = {}
+        dp=[[0,0] for i in range(0,len(prices)+2)]
         
-        def dfs(i,hold):
-            
-            # Base Cases
-            if i>=n:
-                return 0
-            if (i,hold) in dp:
-                return dp[(i,hold)]
-            
-            # If holding stock, we can sell it and wait the coldown.
-            if hold:
-                action = dfs(i+2,not hold) + prices[i]
-                
-            # If not holding stock, we can buy it.
-            else:
-                action = dfs(i+1,not hold) - prices[i]
-                
-            # We can wait.
-            wait = dfs(i+1, hold)
-            
-            # Update the hashmap with maximum of selling/buying and waiting.
-            dp[(i,hold)] = max(action,wait)
-            return dp[(i,hold)]
-    
-        return dfs(0,False)
+        for i in range(len(prices)-1,-1,-1):
+            dp[i][0]=max(dp[i+1][1]-prices[i],dp[i+1][0])
+            dp[i][1]=max(dp[i+2][0]+prices[i],dp[i+1][1])
+        
+        return dp[0][0]
