3e43f67347a2b8a6cc2798ca4a372f2255c90ae8
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-07-07 02:48:05 +0530

    Time: 1248 ms (45.26%), Space: 21.2 MB (83.49%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,25 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         
-        n = len(prices)
-        dp = {}
-        
-        def dfs(i,hold):
-            
-            # Base Cases
-            if i>=n:
-                return 0
-            if (i,hold) in dp:
-                return dp[(i,hold)]
-            
-            # If holding stock, we can sell it and wait the coldown.
-            if hold:
-                dp[(i,hold)] = max(dfs(i+1,not hold) + prices[i] - fee, dfs(i+1,hold))
-                
-            # If not holding stock, we can buy it.
-            else:
-                dp[(i,hold)] = max(dfs(i+1,not hold) - prices[i], dfs(i+1,hold))
-                
-            return dp[(i,hold)]
-    
-        return dfs(0,False)
+        curr, res = prices[0] + fee, 0
+        for i in prices[1:]:
+            if i > curr:
+                res += i - curr
+                curr = i
+            elif curr > i + fee:
+                curr = i + fee
+        return res
