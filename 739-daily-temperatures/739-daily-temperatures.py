f8574ec36a72c8465e1c75f77909f7c345a8d438
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-07-07 23:02:27 +0530

    Time: 2067 ms (25.83%), Space: 25 MB (49.90%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -1,12 +1,19 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+    
         n=len(temperatures)
         answer=[0]*n
         stack=[]
+        
+        # Find index of NGE for each element.
         for i in range(n-1,-1,-1):
+            
             while stack and temperatures[stack[-1]]<=temperatures[i]:
                 stack.pop()
             if stack:
+                # Find no. of days by calculating distance between current index and index of NGE
                 answer[i]=stack[-1]-i
+                
             stack.append(i)
+            
         return answer
