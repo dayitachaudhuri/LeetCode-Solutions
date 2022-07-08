5b59973b2da3b81c48d445096b48d90c8e486986
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-07-08 13:04:33 +0530

    Time: 122 ms (92.71%), Space: 15.9 MB (71.19%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,21 +1,22 @@
 class Solution:
-    def trap(self, heights: List[int]) -> int:
+    def trap(self, height: List[int]) -> int:
         
-        n=len(heights)
-        
-        # Define Left and Right Limit Arrays
-        left=[0]*n
-        right=[0]*n
-        left[0]=heights[0]
-        right[n-1]=heights[n-1]
-        
-        # Find Left and Right Limits
-        for i in range(1,n):
-            left[i]=max(left[i-1],heights[i])
-            right[n-1-i]=max(right[n-i],heights[n-1-i])
-             
-        # Find Volume of water at each index
+        left,right=0,len(height)-1
+        leftMax,rightMax=height[0],height[len(height)-1]
         water=0
-        for i in range(0,n):
-            water+=min(left[i],right[i])-heights[i]
+        
+        while left<right:
+            if height[left]<height[right]:
+                if height[left]>leftMax:
+                    leftMax=height[left]
+                else:
+                    water+=leftMax-height[left]
+                left+=1
+            else:
+                if height[right]>rightMax:
+                    rightMax=height[right]
+                else:
+                    water+=rightMax-height[right]
+                right-=1
+        
         return water
