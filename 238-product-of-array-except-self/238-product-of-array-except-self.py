4df13c77fed7892b994b8faaac7e18d133bf7047
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-07-08 15:45:07 +0530

    Time: 249 ms (87.51%), Space: 21.2 MB (56.20%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -2,16 +2,16 @@ class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         
         n=len(nums)
-        prefix=[1]*n
+        answer=[1]*n
         
         # Find prefix product in one pass
         for i in range(1,n):
-            prefix[i]=prefix[i-1]*nums[i-1]
+            answer[i]=answer[i-1]*nums[i-1]
         
         # Multiply sufix in second pass
         prod=1
         for i in range(n-1,-1,-1):
-            prefix[i]=prefix[i]*prod
+            answer[i]=answer[i]*prod
             prod=prod*nums[i]
         
-        return prefix
+        return answer
