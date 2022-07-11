f7438b4c116b5686fda8610b746978cf1e6e009c
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-07-11 13:37:32 +0530

    Time: 179 ms (69.09%), Space: 16.8 MB (20.04%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,6 +1,14 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        xor=0
+        
+        cache={}
+        
         for i in nums:
-            xor=xor^i
-        return xor
+            if i in cache:
+                cache[i]+=1
+            else:
+                cache[i]=1
+                
+        for val in cache:
+            if cache[val]==1:
+                return val
