97ab618d410221230b905a0b46ba22edcb7177f8
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-07-20 01:11:39 +0530

    Time: 377 ms (12.84%), Space: 18 MB (42.44%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -4,15 +4,39 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    
+    def merge(self, headA, headB):
+            curr=itr=ListNode()
+            while headA and headB:
+                if headA.val<=headB.val:
+                    itr.next=ListNode(headA.val)
+                    headA=headA.next
+                else:
+                    itr.next=ListNode(headB.val)
+                    headB=headB.next
+                itr=itr.next
+            while headA:
+                itr.next=ListNode(headA.val)
+                headA=headA.next
+                itr=itr.next
+            while headB:
+                itr.next=ListNode(headB.val)
+                headB=headB.next
+                itr=itr.next
+            return curr.next
+        
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        nodes=[]
-        for head in lists:
-            while head:
-                nodes.append(head.val)
-                head=head.next
-        nodes.sort()
-        curr=itr=ListNode()
-        for v in nodes:
-            itr.next=ListNode(v)
-            itr=itr.next
-        return curr.next
+        
+        if len(lists)==0:
+            return None
+        if len(lists)==1:
+            return lists[0]
+        
+        temp=[]
+        for i in range(0,len(lists)//2):
+            temp.append(self.merge(lists.pop(), lists.pop()))
+        if lists:
+            temp.append(lists.pop())
+        return self.mergeKLists(temp)
+            
+            
