f79fb7459c77626e69f2afccd9196b99876decf2
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-07-21 00:46:29 +0530

    Time: 965 ms (37.75%), Space: 15.8 MB (32.11%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
@@ -1,21 +1,15 @@
 class Solution:
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
-        bucket={}
+        bucket=defaultdict(lambda:[])
         count=0
         for word in words:
-            if word[0] in bucket:
-                bucket[word[0]].append(word)
-            else:
-                bucket[word[0]]=[word]
+            bucket[word[0]].append(word)
+            
         for char in s:
-            if char in bucket:
-                for i in range(0,len(bucket[char])):
-                    word=bucket[char].pop(0)
-                    if len(word)<=1:
-                        count+=1
-                    else:
-                        if word[1] in bucket:
-                            bucket[word[1]].append(word[1:])
-                        else:
-                             bucket[word[1]]=[word[1:]]
+            for i in range(0,len(bucket[char])):
+                word=bucket[char].pop(0)
+                if len(word)<=1:
+                    count+=1
+                else:
+                    bucket[word[1]].append(word[1:])
         return count
