f5bf18ff160406485370b1a96d8f965c88eef065
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-07-22 22:04:51 +0530

    Time: 545 ms (5.02%), Space: 15.5 MB (71.18%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        n=numCourses
+        adj=[[] for i in range(0,n)]
+        indegree=[0]*n
+        for pair in prerequisites:
+            indegree[pair[0]]+=1
+            adj[pair[1]].append(pair[0])
+        courses=[]
+        ans=True
+        while ans:
+            ans=False
+            for i in range(0,n):
+                if indegree[i]==0:
+                    courses.append(i)
+                    indegree[i]=-1
+                    for vertex in adj[i]:
+                        indegree[vertex]-=1
+                    ans=True
+                    break
+        if len(courses)==n:
+            return courses
+        else:
+            return []
