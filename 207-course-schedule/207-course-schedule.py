d7a7dd284f08f0356a846f1355fc650c0ffc4012
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-07-23 17:52:07 +0530

    Time: 159 ms (48.07%), Space: 17.1 MB (25.63%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,25 +1,25 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        n=numCourses
-        adj=[[] for i in range(0,n)]
-        indegree=[0]*n
+        adj=[[] for i in range(0,numCourses)]
+        indegree=[0 for i in range(0,numCourses)]
+        visited=[0 for i in range(0,numCourses)]
+        rec=[0 for i in range(0,numCourses)]
         for pair in prerequisites:
-            indegree[pair[0]]+=1
             adj[pair[1]].append(pair[0])
-        courses=[]
-        ans=True
-        while ans:
-            ans=False
-            for i in range(0,n):
-                if indegree[i]==0:
-                    courses.append(i)
-                    indegree[i]=-1
-                    for vertex in adj[i]:
-                        indegree[vertex]-=1
-                    ans=True
-                    break
-        if len(courses)==n:
-            return True
-        else:
+            indegree[pair[0]]+=1
+        def dfs(node, rec):
+            visited[node]=1
+            rec[node]=1
+            for nextNode in adj[node]:
+                if  visited[nextNode]==0:
+                    if dfs(nextNode, rec):
+                        return True
+                elif rec[nextNode]==1:
+                    return True
+            rec[node]=0
             return False
+        for course in range(0,numCourses):
+            if dfs(course,rec):
+                return False
+        return True
                     
