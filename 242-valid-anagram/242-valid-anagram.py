4ec1d7b042b28a4b2cf0e07ce1d761c0c35982cd
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-07-29 01:45:34 +0530

    Time: 87 ms (35.62%), Space: 15.1 MB (11.53%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,18 +1,6 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        cacheS={}
-        for i in s:
-            if i in cacheS:
-                cacheS[i]+=1
-            else:
-                cacheS[i]=1
-        cacheT={}
-        for i in t:
-            if i in cacheT:
-                cacheT[i]+=1
-            else:
-                cacheT[i]=1     
-        
-        if cacheS==cacheT:
+        if sorted(s)==sorted(t):
             return True
-        return False
+        else:
+            return False
