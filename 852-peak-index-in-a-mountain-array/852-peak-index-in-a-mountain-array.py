06f2f9700cec8b8a53248d611cb8e283db282594
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-08-06 23:28:59 +0530

    Time: 969 ms (18.31%), Space: 27.2 MB (37.78%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
@@ -1,12 +1,12 @@
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
-        l,r=0,len(arr)-1
+        n=len(arr)
+        l, r = 0, n-1
         while l<=r:
             mid=(l+r)//2
-            if mid<len(arr)-1 and arr[mid]>arr[mid+1]:
-                if mid>0 and arr[mid]>arr[mid-1]:
-                    return mid
-                else:
-                    r=mid
+            if mid==0 or (mid!=n-1 and arr[mid]<arr[mid+1]):
+                l=mid+1
+            elif mid==n-1 or (mid!=0 and arr[mid]<arr[mid-1]):
+                r=mid-1
             else:
-                l=mid
+                return mid
