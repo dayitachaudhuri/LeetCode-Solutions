e8e7cac31b5ed158991ab7aacef9bc0e4a7f45e6
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-08-11 10:15:15 +0530

    Time: 765 ms (30.10%), Space: 27.5 MB (23.98%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
@@ -1,12 +1,25 @@
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        
         n=len(arr)
         l, r = 0, n-1
+        
+        # Binary Search 
+        
         while l<=r:
+            
             mid=(l+r)//2
+            
+            # If mid is peak
+            if mid!=0 and mid!=n-1 and arr[mid]>arr[mid-1] and arr[mid]>arr[mid+1]:
+                return mid
+            
+            # If mid is in slope up
             if mid==0 or (mid!=n-1 and arr[mid]<arr[mid+1]):
                 l=mid+1
-            elif mid==n-1 or (mid!=0 and arr[mid]<arr[mid-1]):
-                r=mid-1
+            
+            # If mid is in slope down
             else:
-                return mid
+                r=mid-1
+                
+            
