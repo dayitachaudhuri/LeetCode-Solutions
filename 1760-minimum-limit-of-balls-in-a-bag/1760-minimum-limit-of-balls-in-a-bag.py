6aa6578710c5101a2af3012fe321817dab29b51b
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-08-11 22:47:55 +0530

    Time: 4036 ms (12.44%), Space: 27.3 MB (5.72%) - LeetHub

---
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minimumSize(self, nums: List[int], maxOperations: int) -> int:
+        
+        def bags(capacity, nums):
+            count=0
+            for val in nums:
+                if val<=capacity:
+                    continue
+                if val%capacity==0:
+                    count+=val//capacity - 1
+                else:
+                    count+=val//capacity
+            return count
+        
+        left=1
+        right=1
+        for n in nums:
+            if n>right:
+                right=n
+        
+        while left<=right:
+            mid=(left+right)//2
+            if bags(mid,nums)>maxOperations:
+                left=mid+1
+            else:
+                right=mid-1
+        return left
