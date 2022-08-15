22ff708fba18fbb1ef9f0be78dec821e064643ca
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-08-16 00:12:45 +0530

    Time: 854 ms (41.00%), Space: 17.2 MB (7.61%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -1,33 +1,42 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
         
-        def possible(capacity):
-            curr, d = 0,0
-            for wt in weights:
-                if curr + wt > capacity:
+        # Function to Calculate Number of Days
+        
+        def calculateDays(capacity):
+            d, curr = 0, 0
+            for w in weights:
+                # If weight exceeds capacity
+                if curr + w > capacity:
                     d += 1
                     curr = 0
-                curr += wt
+                curr += w
+            # If weight remains
             if curr > 0:
-                d += 1
+                d+=1
             return d
         
-        maxWeight, sumWeight = 0,0
-        for wt in weights:
-            if wt > maxWeight:
-                maxWeight = wt
-            sumWeight += wt
+        # Find left and right extremes
         
-        left = maxWeight
-        right = sumWeight
+        left, right = 0, 0
+        for w in weights:
+            if w > left:
+                left = w
+            right += w
+            
+        # Binary Search
         
         while left <= right:
+            
             mid = (left + right) // 2
-            req = possible(mid)
-            if req > days:
+            
+            # If days is exceeding given limit, increase the chosen capacity.
+            if calculateDays(mid)>days:
                 left = mid + 1
+            
+            # If days is less than given limit, decrease the chosen capacity.
             else:
                 right = mid - 1
         
+        # Final value of left at which we exit the loop is the solution
         return left
-                    
