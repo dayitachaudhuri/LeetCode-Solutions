00f1f62a7917896f983534c2e418c51ef882e072
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-08-18 23:43:15 +0530

    Time: 926 ms (55.14%), Space: 32.3 MB (67.22%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -1,15 +1,11 @@
 class Solution:
     def minSetSize(self, arr: List[int]) -> int:
-        total_len = len(arr)
-        target_len = total_len // 2
-        counter = 0
-        arr_chr_count = Counter(arr).most_common()
-        for x in arr_chr_count:
-            total_len -= x[1]
-            counter += 1
-            print(counter)
-            if total_len <= target_len:
-                break
-
-        return counter
+        count = Counter(arr)
+        currentCount = 0
+        currentSize = 0
+        for value in sorted(count.values(), key=None, reverse=True):
+            currentSize += 1
+            if currentCount + value >= len(arr) // 2:
+                return currentSize
+            currentCount += value
         
