b3fa1eecd3c9424b59346ea1327b411ca6a8d43c
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-08-24 00:35:07 +0530

    Time: 208 ms (10.48%), Space: 13.9 MB (99.92%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -1,35 +1,28 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        
         n=len(nums1)
         m=len(nums2)
         count=0
         i,j=0,0
-        curr=0
-        prev=0
+        arr=[]
         
-        while count<=(n+m)//2 and j<m and i<n:
-            prev=curr
+        while j<m and i<n:
             if nums1[i]>nums2[j]:
-                curr=nums2[j]
+                arr.append(nums2[j])
                 j+=1
             else:
-                curr=nums1[i]
+                arr.append(nums1[i])
                 i+=1
-            count+=1
-            
-        if count<=(n+m)//2:
-            prev=curr
-            if j<m:
-                j+=(n+m)//2-count
-                curr=nums2[j]
-                if j!=0:
-                    prev=max(prev,nums2[j-1])
-            else:
-                i+=(n+m)//2-count
-                curr=nums1[i]
-                if i!=0:
-                    prev=max(prev,nums1[i-1])
+        
+        while j<m:
+            arr.append(nums2[j])
+            j+=1
+        
+        while i<n:
+            arr.append(nums1[i])
+            i+=1
         
         if (n+m)%2==0:
-            curr=(curr+prev)/2
-        return curr
+            return (arr[(n+m)//2]+arr[(n+m)//2-1])/2
+        return arr[(n+m)//2]
