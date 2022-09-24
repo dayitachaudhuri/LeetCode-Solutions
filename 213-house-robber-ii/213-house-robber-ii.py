1314e317ffdcfa3d1c59ba9c9842965662446ce6
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-09-24 23:10:48 +0530

    Time: 76 ms (5.71%), Space: 13.8 MB (70.97%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -1,27 +1,18 @@
 class Solution:
     def rob(self, arr: List[int]) -> int:
+        
+        def findmax(k,n):
+            if n==k+1:
+                return arr[k]
+            prev2=arr[k]
+            prev1=max(arr[k],arr[k+1])
+            for i in range(k+2,n):
+                curr=max(arr[i]+prev2,prev1)
+                prev2=prev1
+                prev1=curr
+            return prev1
+        
         if len(arr)==1:
             return arr[0]
-        dp={}
-        def helper(i,j):
-            if i>j:
-                return 0
-            if i in dp:
-                return dp[i]
-            val=max(arr[i] + helper(i+2,j), helper(i+1,j))
-            dp[i]=val
-            return val
         
-        val1=helper(0,len(arr)-2)
-        dp={}
-        val2=helper(1,len(arr)-1)
-        return max(val1,val2)
-    
-    '''   
-        1 2 3 4 5 6 7
-        |---|
-    
-    
-    rob(i) = money + rob(i+2) , rob(i+1)
-    
-    '''  
+        return max(findmax(0,len(arr)-1),findmax(1,len(arr)))
