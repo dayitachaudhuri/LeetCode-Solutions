e3d2b84e1e376a27f144bafc1f277c5492fadaeb
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2022-09-26 19:50:14 +0530

    Time: 1469 ms (13.75%), Space: 27.2 MB (94.81%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
@@ -8,23 +8,14 @@ class Solution:
         
         while l<=r:
             
-            if l==r:
-                return l
-            
-            if l==r-1:
-                if arr[l]>arr[r]:
-                    return l
-                else:
-                    return r
-            
             mid=(l+r)//2
             
             # If mid is peak
-            if arr[mid]>arr[mid-1] and arr[mid]>arr[mid+1]:
+            if mid!=0 and mid!=n-1 and arr[mid]>arr[mid-1] and arr[mid]>arr[mid+1]:
                 return mid
             
             # If mid is in slope up
-            if arr[mid]<arr[mid+1]:
+            if mid==0 or (mid!=n-1 and arr[mid]<arr[mid+1]):
                 l=mid+1
             
             # If mid is in slope down
